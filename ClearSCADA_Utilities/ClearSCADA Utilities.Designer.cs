5fea49414009fcfe7bc44a8843d2e15d71a1dc74
Joseph Schrag <skaguy25@hotmail.com>
2012-10-29 08:32:35 -0600

    Initial Commit

---
+++ ClearSCADA_Utilities/ClearSCADA Utilities.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ClearSCADA_Utilities
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxID = new System.Windows.Forms.TextBox();
+            this.labelTemplateID = new System.Windows.Forms.Label();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.dataGridViewResults = new System.Windows.Forms.DataGridView();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.labelServer = new System.Windows.Forms.Label();
+            this.checkBoxShowMismatchesOnly = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxID
+            // 
+            this.textBoxID.Location = new System.Drawing.Point(775, 10);
+            this.textBoxID.Name = "textBoxID";
+            this.textBoxID.Size = new System.Drawing.Size(100, 22);
+            this.textBoxID.TabIndex = 2;
+            // 
+            // labelTemplateID
+            // 
+            this.labelTemplateID.AutoSize = true;
+            this.labelTemplateID.Location = new System.Drawing.Point(691, 13);
+            this.labelTemplateID.Name = "labelTemplateID";
+            this.labelTemplateID.Size = new System.Drawing.Size(78, 17);
+            this.labelTemplateID.TabIndex = 1;
+            this.labelTemplateID.Text = "Instance ID";
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(1075, 9);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(75, 23);
+            this.buttonSubmit.TabIndex = 4;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // dataGridViewResults
+            // 
+            this.dataGridViewResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewResults.Location = new System.Drawing.Point(15, 55);
+            this.dataGridViewResults.Name = "dataGridViewResults";
+            this.dataGridViewResults.RowTemplate.Height = 24;
+            this.dataGridViewResults.Size = new System.Drawing.Size(1135, 633);
+            this.dataGridViewResults.TabIndex = 100;
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.Location = new System.Drawing.Point(185, 9);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(73, 17);
+            this.labelUsername.TabIndex = 5;
+            this.labelUsername.Text = "Username";
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Location = new System.Drawing.Point(264, 9);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(100, 22);
+            this.textBoxUsername.TabIndex = 0;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Location = new System.Drawing.Point(382, 9);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(69, 17);
+            this.labelPassword.TabIndex = 7;
+            this.labelPassword.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(457, 9);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(100, 22);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(68, 9);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(100, 22);
+            this.textBoxServer.TabIndex = 5;
+            this.textBoxServer.Text = "localhost";
+            // 
+            // labelServer
+            // 
+            this.labelServer.AutoSize = true;
+            this.labelServer.Location = new System.Drawing.Point(12, 9);
+            this.labelServer.Name = "labelServer";
+            this.labelServer.Size = new System.Drawing.Size(50, 17);
+            this.labelServer.TabIndex = 9;
+            this.labelServer.Text = "Server";
+            // 
+            // checkBoxShowMismatchesOnly
+            // 
+            this.checkBoxShowMismatchesOnly.AutoSize = true;
+            this.checkBoxShowMismatchesOnly.Location = new System.Drawing.Point(894, 11);
+            this.checkBoxShowMismatchesOnly.Name = "checkBoxShowMismatchesOnly";
+            this.checkBoxShowMismatchesOnly.Size = new System.Drawing.Size(175, 21);
+            this.checkBoxShowMismatchesOnly.TabIndex = 3;
+            this.checkBoxShowMismatchesOnly.Text = "Show Mismatches Only";
+            this.checkBoxShowMismatchesOnly.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 700);
+            this.Controls.Add(this.checkBoxShowMismatchesOnly);
+            this.Controls.Add(this.textBoxServer);
+            this.Controls.Add(this.labelServer);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.dataGridViewResults);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.labelTemplateID);
+            this.Controls.Add(this.textBoxID);
+            this.Name = "Form1";
+            this.Text = "ClearSCADA Utilities";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxID;
+        private System.Windows.Forms.Label labelTemplateID;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.DataGridView dataGridViewResults;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Label labelServer;
+        private System.Windows.Forms.CheckBox checkBoxShowMismatchesOnly;
+    }
+}
+
---
+++ ClearSCADA_Utilities/ClearSCADA Utilities.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ClearScada.Client;
+using ClearScada.Client.Simple;
+
+namespace ClearSCADA_Utilities
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            using (Connection conn = new Connection("myConn"))
+            {
+                conn.Connect(textBoxServer.Text);
+                conn.LogOn(textBoxUsername.Text, textBoxPassword.Text);
+
+                DBObject dbObj = null;
+
+                try
+                {
+                    int id = Convert.ToInt32(textBoxID.Text);
+                    dbObj = conn.GetObject(new ObjectId(id));
+                    if (dbObj == null) throw new Exception("Invalid object ID.");
+                    if (!dbObj.IsGroup) throw new Exception("Object is not a group.");
+                }
+
+                catch (FormatException ex)
+                {
+                    MessageBox.Show("Enter a valid object ID.");
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    return;
+                }
+
+                if (dbObj.ClassDefinition.Name == "CTemplateInstance") ProcessInstance(dbObj);
+                else if (dbObj.ClassDefinition.Name == "CTemplate") ProcessTemplate(dbObj);
+                
+                conn.LogOff();
+                conn.Disconnect();
+            }
+        }
+
+        private void ProcessTemplate(DBObject template)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Fullname");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Template Address");
+
+            //Todo: Find all instances & loop through them adding their addresses to grid
+
+            dataGridViewResults.DataSource = dt;
+        }
+
+        private void ProcessInstance(DBObject instance)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Fullname");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Template Address");
+            dt.Columns.Add("Instance Address");
+
+            AddDescendants(ref dt, instance, "CSCADAPackMAnalogIn");
+            AddDescendants(ref dt, instance, "CSCADAPackMAnalogOut");
+            AddDescendants(ref dt, instance, "CSCADAPackMDigitalIn");
+            AddDescendants(ref dt, instance, "CSCADAPackMDigitalOut");
+
+            dataGridViewResults.DataSource = dt;
+        }
+
+        private void AddDescendants(ref DataTable dt, DBObject dbObj, string dbClass)
+        {
+            DBObjectCollection descendants = dbObj.GetDescendants(dbClass, "");
+            
+            foreach (DBObject descendant in descendants)
+            {
+                DBObject template = descendant.TemplateObject;
+                string instanceAddress = descendant.GetProperty("Address").ToString();
+                string templateAddress = template.GetProperty("Address").ToString();
+                List<string> values = new List<string>();
+                values.Add(template.FullName);
+                values.Add(template.Name);
+                values.Add(templateAddress);
+                values.Add(instanceAddress);
+
+                if (!checkBoxShowMismatchesOnly.Checked || (checkBoxShowMismatchesOnly.Checked && templateAddress != instanceAddress))
+                {
+                    dt.Rows.Add(values.ToArray());
+                }
+            }
+        }
+    }
+}
